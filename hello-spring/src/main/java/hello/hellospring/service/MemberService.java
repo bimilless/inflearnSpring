95465989cf18e6c07860373d339cc72615fe2798
bimilless <bimils7856@gmail.com>
2023-09-23 20:23:31 +0900

    feat: 의존관계 설정

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,10 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.support.MergedBeanDefinitionPostProcessor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService { // ctrl + shift + t = 테스트 클래스 생성
 
     private final MemberRepository memberRepository;
